6754ce9477f68636375e3a9d9abc705b2ecaccaf
N1ckyto <escuderonico0@gmail.com>
2024-05-04 16:03:12 -0300

    MenuResponsive/pages

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,1 +1,5 @@
-// xdxdxdxdxd
+document.querySelector(".btn_menu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    document.querySelector(".LinkBar").classList.toggle("show");
+}
