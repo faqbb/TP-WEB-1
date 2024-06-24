c895380c2bad7100e52053743a1804359d7f4368
N1ckyto <escuderonico0@gmail.com>
2024-06-23 21:17:19 -0300

    resolved table

--- scripts/navPartialRender.js
+++ scripts/navPartialRender.js
@@ -7,6 +7,7 @@ let btn_title = document.querySelector(".btntitleindex");
 btn_title.addEventListener("click", (event) => push(event));
 
 const text = "mundiViajes";
+const apiEndpoint = 'https://6675d4f5a8d2b4d072f19c6b.mockapi.io/web/tp/userExperiences';
 
 let cont = document.querySelector(".mainContainer");
 
@@ -127,11 +128,13 @@ function processText(t) {
     cont.querySelectorAll(".entry")
         .forEach(b => b.addEventListener("click", (event) => pushBlog(event)));
 
+    //seccion api
+    fetchData()
 
     //section register
     displayCaptcha()
     cont.querySelectorAll("#TextCaptcha")
-        .forEach(b => b.addEventListener("click", displayCaptcha));
+        .forEach(b => b.addEventListener("none", displayCaptcha));
     cont.querySelectorAll("#form")
         .forEach(b => b.addEventListener("submit", agregar));
     cont.querySelectorAll("#refreshButton")
@@ -144,6 +147,7 @@ function processText(t) {
             ValueInput();
             CompararArreglos();
         }))
+
 }
 
 //seccion slide
@@ -279,4 +283,180 @@ function ResetInput() {
 }
 
 
+
+async function fetchData() {
+    console.log("entre en la funcion de tabala");
+    try {
+        const response = await fetch(apiEndpoint);
+        if (!response.ok) {
+            throw new Error(`Error! Estado de respuesta: ${response.status}`);
+        }
+        const data = await response.json();
+        let table = document.getElementById('apiTableBody');
+        if (table) {
+            data.forEach(item => {
+                addEditableRow(item.id, item.Destiny, item.Companion, item.Date, table)
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function addEditableRow(id, destiny, companion, date, table) {
+    const newRow = table.insertRow();
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.classList = "tertiaryColor"
+    cell1.innerText = id;
+
+    cell2.classList = "tertiaryColor"
+    cell2.innerText = destiny;
+
+    cell3.classList = "tertiaryColor";
+    cell3.innerText = companion;
+
+    cell4.classList = "tertiaryColor";
+    cell4.innerText = date;
+
+    cell5.innerHTML = '<button onclick="editRow(this)" class="rowButton backgroundPrimary tertiaryColor">Editar</button> <button onclick="deleteRow(this)" class="rowButton backgroundPrimary tertiaryColor">Borrar</button> <button onclick="sendToApi(this)" class="rowButton backgroundPrimary tertiaryColor">Enviar</button>';
+}
+
+async function addFromInputs() {
+    let destiny = document.getElementById("destinyInput").value
+    let companion = document.getElementById("companionInput").value
+    let date = document.getElementById("dateInput").value
+    let table = document.getElementById('apiTableBody');
+    if (table && destiny && companion && date) {
+        const data = {
+            Destiny: destiny,
+            Companion: companion,
+            Date: date
+        };
+        await postToApi(data)
+        addEditableRow(data.id, data.Destiny, data.Companion, data.Date, table)
+    }
+}
+
+
+async function sendToApi(button) {
+    try {
+        const row = button.parentNode.parentNode;
+        const cell1 = row.cells[0].innerText;
+        const cell2 = row.cells[1].innerText;
+        const cell3 = row.cells[2].innerText;
+        const cell4 = row.cells[3].innerText;
+
+        const data = {
+            id: cell1,
+            Destiny: cell2,
+            Companion: cell3,
+            Date: cell4
+        };
+        let id = await isExistingData(data)
+        console.log(id)
+        if (id >= 0) {
+            putInApi(data, id)
+        } else {
+            postToApi(data)
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+async function postToApi(data) {
+    try {
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error(`Error! Estado de respuesta: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log('Respuesta de la api:', result);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function putInApi(data, id) {
+    const updateResponse = await fetch(`${apiEndpoint}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    if (!updateResponse.ok) {
+        throw new Error(`Error! Estado de respuesta: ${updateResponse.status}`);
+    }
+    const updateResult = await updateResponse.json();
+    console.log('Datos actualizados:', updateResult);
+    return true;
+}
+
+async function isExistingData(rowData) {
+    try {
+        let existingId = -1;
+        const response = await fetch(apiEndpoint);
+        if (!response.ok) {
+            throw new Error(`Error! Estado de respuesta: ${response.status}`);
+        }
+        const data = await response.json();
+        for (let item of data) {
+            if (item.id === rowData.id) {
+                existingId = item.id;
+                break;
+            }
+        }
+        return existingId;
+    } catch (e) {
+        console.error(e);
+        return existingId;
+    }
+}
+
+async function deleteData(id) {
+    try {
+        const response = await fetch(`${apiEndpoint}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            console.log(`Elemento con ID ${id} eliminado correctamente.`);
+        } else {
+            console.error(`Error! Estado de respuesta: ${updateResponse.status}`);
+        }
+    } catch (e) {
+        console.error('Se produjo un error al intentar eliminar el elemento:', e);
+    }
+}
+
+async function deleteRow(button) {
+    const row = button.parentNode.parentNode;
+    const id = row.cells[0].innerText;
+    await deleteData(id)
+    row.parentNode.removeChild(row);
+}
+
+function editRow(button) {
+    const row = button.parentNode.parentNode;
+    const cells = row.getElementsByTagName('td');
+    for (let i = 0; i < cells.length - 1; i++) {
+        cells[i].contentEditable = cells[i].contentEditable === "true" ? "false" : "true";
+        button.innerText = cells[i].contentEditable === "true" ? "Dejar de editar" : "Editar"
+    }
+}
 
